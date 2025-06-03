6220c13961daed774252aa286b78c380165ef8b9
minjae <minj7292@naver.com>
2025-06-03 14:22:36 +0900

    문제 조회 기능 추가

--- src/models/Room.js
+++ src/models/Room.js
@@ -1,14 +1,24 @@
 import mongoose from "mongoose";
 
+// 개별 질문 스키마 정의
+const questionSchema = new mongoose.Schema({
+  question_text: { type: String, required: true },
+  correct_answer: { type: String, required: true },
+});
+
 const RoomSchema = new mongoose.Schema({
-  host: String,
-  roomCode: String,
-  createdAt: Date,
-  nicknames: [String],
+  host: { type: String, default: "default" },
+  roomCode: { type: String, required: true, unique: true },
+  createdAt: { type: Date, default: Date.now },
+  nicknames: { type: [String], default: [] },
   participants: {
     type: Map,
     of: Number,
     default: {},
+  },
+  questions: {
+    type: [questionSchema], // ✅ 문제 배열 추가
+    default: [],
   },
 });
 
